6fb9b98cbcbe0c3822cb8bae5d01b6755da79d27
champix56 <desorbaix@free.fr>
2025-11-05 16:09:33 +0100

    jour 2

---
+++ horloge/horloge/horloge.ino
@@ -0,0 +1,60 @@
+//YWROBOT
+//Compatible with the Arduino IDE 1.0
+//Library version:1.1
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+#include <TimeLib.h>
+#include <DS1307RTC.h>
+// Include the libraries we need
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS 14
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+uint8_t CHAR_DEG[8]={
+  B11100,
+  B10100,
+  B11100,
+  0x0,0x0,0x0,0X0};
+void setup() {
+  lcd.init();  // initialize the lcd
+  lcd.createChar(0,CHAR_DEG);
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(3, 0);
+  lcd.print("Demat breizh");
+  delay(3000);    
+  lcd.clear();
+  lcd.setCursor(14,1);
+  lcd.print('C');
+    sensors.begin();
+  
+}
+void loop() {
+   tmElements_t tm;
+
+  if (RTC.read(tm)) {
+    lcd.setCursor(4,0);
+    lcd.print(tm.Hour);    lcd.print(':');    lcd.print(tm.Minute);    lcd.print(':');    lcd.print(tm.Second);
+  }
+ sensors.requestTemperatures(); // Send the command to get temperatures
+ float tempC = sensors.getTempCByIndex(0);
+
+  // Check if reading was successful
+  if (tempC != DEVICE_DISCONNECTED_C)
+  {
+        lcd.setCursor(9,1);
+        lcd.print("     "); 
+        lcd.setCursor(9,1);
+        lcd.print(tempC);
+  }
+  delay(500);
+}
--- led-pot-button/led-pot-button.ino
+++ led-pot-button/led-pot-button.ino
@@ -4,26 +4,60 @@
 #define PIN_BUTTON 2
 #define PIN_POT A0
 int valeurDuPotentiometre;
-volatile bool isClicked=false;
-void onButtonClicked(){
-  isClicked=true;
+volatile bool isClicked = false;
+void onButtonClicked() {
+  isClicked = true;
 }
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(PIN_GREEN, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("initialization en cours");
+
+  Serial.print("Saisir votre nom:");
+  while (Serial.available() == 0) { delay(500); }
+  String prenom=Serial.readString();
+  Serial.print("Bonjour ");
+  Serial.println(prenom); 
+
+
+  /*char buffer[32];
+  char itter = 0;
+  do {
+    buffer[itter++] = Serial.read();
+  } while (itter < 31 && Serial.available());
+  Serial.flush();
+  buffer[itter] = '\0';*/
+  
+  
+   pinMode(PIN_GREEN, OUTPUT);
+  Serial.print('.');
   pinMode(PIN_ORANGE, OUTPUT);
+  Serial.print('.');
   pinMode(PIN_RED, OUTPUT);
+  Serial.print('.');
   pinMode(PIN_BUTTON, INPUT);
+  Serial.print('.');
   attachInterrupt(digitalPinToInterrupt(PIN_BUTTON), onButtonClicked, RISING);
+  Serial.println('.');
   valeurDuPotentiometre = map(analogRead(PIN_POT), 0, 1023, 0, 255);
+  Serial.print(valeurDuPotentiometre, DEC);
+  Serial.print(' ');
+  Serial.print(valeurDuPotentiometre, HEX);
+  Serial.print(' ');
+  Serial.print(valeurDuPotentiometre, OCT);
+  Serial.print(' ');
+  Serial.print(valeurDuPotentiometre, BIN);
+  Serial.println("\nAllumage feu vert");
+  Serial.print('c');
+  Serial.write(67);
   analogWrite(PIN_GREEN, valeurDuPotentiometre);
 }
 
 void loop() {
   if (isClicked == true) {
     sequencePieton(800);
-    isClicked=false;
+    isClicked = false;
   } else {
     delay(1000);
   }
